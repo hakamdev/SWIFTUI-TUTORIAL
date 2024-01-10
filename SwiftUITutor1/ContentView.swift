be3bd78695bd7830cbeafc50ed015c232bf4f653
hakamdev <abdouhakam8@gmail.com>
2024-01-10 12:17:49 +0100

    Initial Commit

---
+++ SwiftUITutor1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUITutor1
+//
+//  Created by Hakam Dev on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUITutor1/SwiftUITutor1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutor1App.swift
+//  SwiftUITutor1
+//
+//  Created by Hakam Dev on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutor1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
